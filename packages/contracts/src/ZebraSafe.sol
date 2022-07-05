e3ee4258bb2f79466b2b124a89d89cfe60afcda7
npasquie <nicpas@live.fr>
2022-07-04 23:01:20 -0400

    ➕  gnosis safe custom implem added

--- packages/contracts/src/ZebraSafe.sol
+++ packages/contracts/src/ZebraSafe.sol
@@ -8,6 +8,35 @@ import "gnosis-safe/GnosisSafe.sol";
 /// @author npasquie
 contract ZebraSafe is GnosisSafe {
 
+
+ function setup(
+        address[] calldata _owners,
+        uint256 _threshold,
+        address to,
+        bytes calldata data,
+        address fallbackHandler,
+        address paymentToken,
+        uint256 payment,
+        address payable paymentReceiver
+    ) override external {
+        // setupOwners checks if the Threshold is already set, therefore preventing that this method is called twice
+        setupOwners(_owners, _threshold);
+        if (fallbackHandler != address(0)) internalSetFallbackHandler(fallbackHandler);
+        // As setupOwners can only be called if the contract has not been initialized we don't need a check for setupModules
+        setupModules(to, data);
+
+        // if (payment > 0) {
+        //     // To avoid running into issues with EIP-170 we reuse the handlePayment function (to avoid adjusting code of that has been verified we do not adjust the method itself)
+        //     // baseGas = 0, gasPrice = 1 and gas = payment => amount = (payment + 0) * 1 = payment
+        //     handlePayment(payment, 0, 1, paymentToken, paymentReceiver);
+        // }
+        emit SafeSetup(msg.sender, _owners, _threshold, to, fallbackHandler);
+    }
+
+
+
+
+
     // todo : 
     // {
     //     bytes32 slot = GUARD_STORAGE_SLOT;
