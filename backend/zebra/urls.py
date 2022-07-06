28439c4782f2941398b1120d77cd45f5170fbefc
Tony Dattolo <anthonydattolo@gmail.com>
2022-07-05 23:04:11 -0400

    url api endpoints

--- backend/zebra/urls.py
+++ backend/zebra/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 
+from .views import *
 
 urlpatterns = [
-
+    path('list/', ZebraNFTListView.as_view(), name='zebra-nft-list'),
+    path('create/', CreateZebraNFTView.as_view(), name='zebra-nft-create'),
+    path('read/', ReadZebraNFTView.as_view(), name='zebra-nft-read'),
+    path('update/', UpdateZebraNFTView.as_view(), name='zebra-nft-update'),
+    path('delete/', DeleteZebraNFTView.as_view(), name='zebra-nft-delete'),
 ]
