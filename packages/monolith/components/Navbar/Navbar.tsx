848d12eff66965895096ea0046ca7a3f0517a47f
skrider <skrider@berkeley.edu>
2022-07-06 21:57:14 -0400

    update Navbar

--- packages/monolith/components/Navbar/Navbar.tsx
+++ packages/monolith/components/Navbar/Navbar.tsx
@@ -1,56 +1,96 @@
-import React, { RefObject } from "react";
+import React from "react";
 import {
   HStack,
   Grid,
-  Box,
   Button,
-  Avatar,
-  useDisclosure,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
 } from "@chakra-ui/react";
-// import { MockCountryPicker, TeamIcon } from "../Icons";
-// import LinkButton from "../LinkButton";
-// import UserSidebar from "../UserSidebar";
-// import ConnectModal from "../ConnectModal";
-// import ConnectButton from "../ConnectButton";
-import ConnectButton from "../ConnectButton/ConnectButton";
+import { ChevronDownIcon } from "@chakra-ui/icons";
+import LinkButton from "../LinkButton";
+import { useRouter } from "next/router";
+import { ZebraText } from "../Icons";
+import EthereumIcon from "../Icons/EthereumIcon";
 
-import Link from "next/link";
-
-const Navbar = () => (
-  <Grid
-    as="nav"
-    w="100%"
-    color="white"
-    gridTemplateColumns="1fr 1fr 1fr"
-    px="27px"
-    my={18}
-  >
-    <Link href="/">
-      {/* <TeamIcon w={20} h="auto" color="white" />
-       */}
-      <p>ZEBRA</p>
-    </Link>
-    <Box placeSelf="center">
-      {/* <LinkButton
-          href="/"
-          variant="text"
-          mr="2"
-          activeProps={{ variant: "primary" }}
-        >
-          Guilds
+const Navbar = () => {
+  const router = useRouter();
+  return (
+    <>
+      <Grid
+        w="100%"
+        color="white"
+        gridTemplateColumns="1fr 1fr 1fr"
+        px="27px"
+        my={18}
+      >
+        <LinkButton href="/" placeSelf="center start" variant="unstyled">
+          <ZebraText w="106px" h="43px" />
         </LinkButton>
-        <LinkButton
-          href="/documentation"
-          variant="text"
-          activeProps={{ variant: "primary" }}
-        >
-          Documentation
-        </LinkButton> */}
-    </Box>
-    <HStack alignItems="center" justifyContent="flex-end">
-      <ConnectButton />
-    </HStack>
-  </Grid>
-);
+        <HStack placeSelf="center" spacing="3">
+          <LinkButton
+            href="/games"
+            variant="ghost"
+            activeProps={{ variant: "primary" }}
+          >
+            Browse Games
+          </LinkButton>
+          <LinkButton
+            href="/supply"
+            variant="ghost"
+            activeProps={{ variant: "primary" }}
+          >
+            Supply
+          </LinkButton>
+          <LinkButton
+            href="/borrow"
+            variant="ghost"
+            activeProps={{ variant: "primary" }}
+          >
+            Borrowing
+          </LinkButton>
+        </HStack>
+        <HStack alignItems="center" justifyContent="flex-end" spacing="3">
+          <Menu>
+            <MenuButton
+              variant="unstyled"
+              as={Button}
+              leftIcon="🇺🇸"
+              rightIcon={
+                <ChevronDownIcon transform="scale(1.3) translate(0, 1px)" />
+              }
+            >
+              US
+            </MenuButton>
+            <MenuList>
+              <MenuItem>🇺🇸 US</MenuItem>
+            </MenuList>
+          </Menu>
+          <Menu>
+            <MenuButton
+              variant="unstyled"
+              as={Button}
+              leftIcon={<EthereumIcon />}
+              rightIcon={
+                <ChevronDownIcon transform="scale(1.3) translate(0, 1px)" />
+              }
+            >
+              ETH
+            </MenuButton>
+            <MenuList>
+              <MenuItem>
+                <EthereumIcon /> ETH
+              </MenuItem>
+            </MenuList>
+          </Menu>
+          <Button marginLeft="20px" variant="secondary">
+            Connect
+          </Button>
+        </HStack>
+      </Grid>
+    </>
+  );
+};
 
 export default Navbar;
