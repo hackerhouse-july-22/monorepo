636b8fcf356ba5502049f88379c96cc08c11a4a7
Caelin Sutch <32991829+caelinsutch@users.noreply.github.com>
2022-07-06 20:54:43 -0400

    [HAC-14] Create renting onboarding page + refactor to use page container instead of global layout for meta (#8)

---
+++ packages/monolith/components/PageContainer/PageContainer.tsx
@@ -0,0 +1,48 @@
+import React, { ReactNode } from "react";
+import Head from "next/head";
+import { Box } from "@chakra-ui/react";
+import Navbar from "../Navbar/Navbar";
+import { useRouter } from "next/router";
+
+type PageContainerProps = {
+  title?: string;
+  description?: string;
+  imageUrl?: string;
+  children?: ReactNode;
+};
+
+const PageContainer: React.FC<PageContainerProps> = ({
+  title = "Zebra",
+  description = "Zebra Time",
+  imageUrl = "",
+  children,
+}) => {
+  const { asPath } = useRouter();
+  const url = `http://localhost:3000/${asPath}`;
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>{title}</title>
+        <meta name="title" content={title} />
+        <meta name="description" content={description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={imageUrl} />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={url} />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content={imageUrl} />
+        <link rel="canonical" href={url} />
+      </Head>
+
+      <Navbar />
+      <Box as="main">{children}</Box>
+    </>
+  );
+};
+
+export default PageContainer;
---
+++ packages/monolith/pages/onboarding/renting.tsx
@@ -0,0 +1,23 @@
+import { NextPage } from "next";
+import { Button, Container, Heading, Link, Text } from "@chakra-ui/react";
+import PageContainer from "@/components/PageContainer";
+
+const OnboardingRenting: NextPage = () => (
+  <PageContainer>
+    <Container textAlign="center" mx="auto">
+      <Heading as="h1" size="2xl">
+        Let's setup your renting account...
+      </Heading>
+      <Text mt={4} maxW="600px" mx="auto">
+        Behind the scenes, we use a tool called a{" "}
+        <Link href="https://gnosis-safe.io/">Gnosis Safe</Link> when you’re
+        renting NFT’s, lets create one for you so you can start renting NFT’s!
+      </Text>
+      <Button colorScheme="pink" mt={6}>
+        Create Gnosis Safe
+      </Button>
+    </Container>
+  </PageContainer>
+);
+
+export default OnboardingRenting;
