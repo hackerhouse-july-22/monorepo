063794fdb3bc4eb8f648ee3626c6f33b97cdce46
skrider <skrider@berkeley.edu>
2022-07-09 17:00:18 -0400

    auth working for next routes

--- packages/monolith/pages/api/login.tsx
+++ packages/monolith/pages/api/login.tsx
@@ -5,41 +5,47 @@ import { LoginPayload } from "@thirdweb-dev/sdk/dist/src/schema";
 
 const login = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== "POST") {
-    return res.status(400).json({ 
-      error: "Invalid method. Only POST supported." 
+    return res.status(400).json({
+      error: "Invalid method. Only POST supported.",
     });
   }
 
   const PRIVATE_KEY = process.env.ADMIN_PRIVATE_KEY;
   if (!PRIVATE_KEY) {
     console.error("Missing ADMIN_PRIVATE_KEY environment variable");
-    return res.status(500).json({ 
-      error: "Admin private key not set" 
-    })
+    return res.status(500).json({
+      error: "Admin private key not set",
+    });
   }
 
-  const sdk = ThirdwebSDK.fromPrivateKey(process.env.ADMIN_PRIVATE_KEY as string, "mainnet");
-  
+  const sdk = ThirdwebSDK.fromPrivateKey(
+    process.env.ADMIN_PRIVATE_KEY as string,
+    "mainnet"
+  );
+
   // Get signed login payload from the frontend
   const payload = req.body.payload as LoginPayload;
   if (!payload) {
-    return res.status(400).json({ 
-      error: "Must provide a login payload to generate a token" 
-    })
-  }  
+    return res.status(400).json({
+      error: "Must provide a login payload to generate a token",
+    });
+  }
 
   // Generate an access token with the SDK using the signed payload
-  const domain = "thirdweb.com"
+  const domain = process.env.NEXT_PUBLIC_SERVER_DOMAIN!;
   const token = await sdk.auth.generateAuthToken(domain, payload);
 
   // Securely set httpOnly cookie on request to prevent XSS on frontend
   // And set path to / to enable access_token usage on all endpoints
-  res.setHeader("Set-Cookie", serialize("access_token", token, {
-    path: "/",
-    httpOnly: true,
-    secure: true,
-    sameSite: "strict",
-  }));
+  res.setHeader(
+    "Set-Cookie",
+    serialize("access_token", token, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+    })
+  );
 
   res.status(200).json("Successfully logged in.");
 };
